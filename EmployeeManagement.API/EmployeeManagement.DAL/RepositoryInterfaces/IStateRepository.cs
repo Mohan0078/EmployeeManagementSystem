f07ef11075375f22044ee1f8296841a150c7be1a
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-05-11 18:14:07 +0530

    feat: IStateRepository methods

---
+++ EmployeeManagement.API/EmployeeManagement.DAL/RepositoryInterfaces/IStateRepository.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeManagement.DatabaseEntities.Models;
+
+namespace EmployeeManagement.DAL.RepositoryInterfaces
+{
+    public interface IStateRepository
+    {
+        Task<bool> AddState(State state);
+        Task<bool> EditState(State state);
+        Task<State> GetStateById(Guid stateId);
+        Task<List<State>> GetAllStates();
+        Task<bool> DeleteState(Guid stateId);
+    }
+}
