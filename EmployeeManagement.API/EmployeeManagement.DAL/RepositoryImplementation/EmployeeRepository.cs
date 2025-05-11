4131f49c3a4aaf5710cb6148f3fdcd950ff7ea62
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-05-11 19:10:21 +0530

    feat: EmployeeRepository methods

---
+++ EmployeeManagement.API/EmployeeManagement.DAL/RepositoryImplementation/EmployeeRepository.cs
@@ -0,0 +1,33 @@
+﻿using EmployeeManagement.DAL.RepositoryInterfaces;
+using EmployeeManagement.DatabaseEntities.Models;
+
+namespace EmployeeManagement.DAL.RepositoryImplementation
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        public Task<bool> AddEmployeeAsync(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteEmployee(Guid employeeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> EditEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Employee>> GetAllEmployees()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> GetEmployeeById(Guid employeeId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
