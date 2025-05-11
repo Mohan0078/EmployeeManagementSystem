32d27de3b86067f55c88836399e4ac1356b38c37
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-05-11 13:00:13 +0530

    feat: employee database entity

---
+++ EmployeeManagement.API/EmployeeManagement.DatabaseEntities/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmployeeManagement.DatabaseEntities.Models
+{
+    public partial class Employee
+    {
+        public Guid EmployeeId { get; set; }
+        public Guid? MemberId { get; set; }
+        public string? Designation { get; set; }
+        public DateTime? DateOfJoin { get; set; }
+        public decimal? Salary { get; set; }
+        public Guid? StateId { get; set; }
+        public string IsDeleted { get; set; } = null!;
+    }
+}
