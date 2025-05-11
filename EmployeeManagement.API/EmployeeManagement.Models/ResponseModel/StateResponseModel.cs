fe3e2f04e0924ae83d092f222ef8bf45eaa2361e
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-05-11 19:59:01 +0530

    feat: state response model

---
+++ EmployeeManagement.API/EmployeeManagement.Models/ResponseModel/StateResponseModel.cs
@@ -0,0 +1,8 @@
+﻿namespace EmployeeManagement.Models.ResponseModel
+{
+    public class StateResponseModel
+    {
+        public Guid StateId { get; set; }
+        public string StateName { get; set; }
+    }
+}
