28c5c94edfce6625a4b98727d938418538d451e5
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-05-11 12:58:00 +0530

    feat: state database entity

---
+++ EmployeeManagement.API/EmployeeManagement.DatabaseEntities/Models/State.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmployeeManagement.DatabaseEntities.Models
+{
+    public partial class State
+    {
+        public Guid StateId { get; set; }
+        public string? StateName { get; set; }
+        public string? StateCode { get; set; }
+        public bool? IsDeleted { get; set; }
+    }
+}
