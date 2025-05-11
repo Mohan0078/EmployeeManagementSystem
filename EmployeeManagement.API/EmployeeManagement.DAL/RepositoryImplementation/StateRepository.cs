b70fb0bed94f1030aef762b783c2a61b67a342ee
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-05-11 18:16:30 +0530

    feat: stateRepository methods

---
+++ EmployeeManagement.API/EmployeeManagement.DAL/RepositoryImplementation/StateRepository.cs
@@ -0,0 +1,33 @@
+﻿using EmployeeManagement.DAL.RepositoryInterfaces;
+using EmployeeManagement.DatabaseEntities.Models;
+
+namespace EmployeeManagement.DAL.RepositoryImplementation
+{
+    internal class StateRepository : IStateRepository
+    {
+        public Task<bool> AddState(State state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteState(Guid stateId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> EditState(State state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<State>> GetAllStates()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<State> GetStateById(Guid stateId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
