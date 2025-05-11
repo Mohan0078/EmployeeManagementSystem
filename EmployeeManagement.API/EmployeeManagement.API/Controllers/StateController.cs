133aea936bb82ed0d5c570dcaf20d1993f063d6c
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-05-11 20:14:13 +0530

    feat: state controller and get state list endpoint

---
+++ EmployeeManagement.API/EmployeeManagement.API/Controllers/StateController.cs
@@ -0,0 +1,31 @@
+﻿using EmployeeManagement.BLL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StateController : ControllerBase
+    {
+        private readonly IStateService _stateService;
+
+        public StateController(IStateService stateService)
+        {
+            _stateService = stateService;
+        }
+
+        [HttpGet("GetStateList")]
+        public async Task<IActionResult> GetStateList()
+        {
+            try
+            {
+              var result = await _stateService.GetStateListAsync();
+              return Ok(result);
+            }
+            catch (Exception ex)
+            {
+              return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);  
+            }
+        }
+    }
+}
