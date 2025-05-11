05e66349d2968b1308073355b2ee7ba3319497f6
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-05-11 12:59:35 +0530

    feat: member database entity

---
+++ EmployeeManagement.API/EmployeeManagement.DatabaseEntities/Models/Member.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmployeeManagement.DatabaseEntities.Models
+{
+    public partial class Member
+    {
+        public Guid MemberId { get; set; }
+        public string? Name { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
