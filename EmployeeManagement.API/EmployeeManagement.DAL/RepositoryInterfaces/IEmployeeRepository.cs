5a5728225fdefdbe798e7e8090a54bc60652fef5
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-05-11 19:08:16 +0530

    feat: IEmployeeRepository methods

---
+++ EmployeeManagement.API/EmployeeManagement.DAL/RepositoryInterfaces/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeManagement.DatabaseEntities.Models;
+
+namespace EmployeeManagement.DAL.RepositoryInterfaces
+{
+    public interface IEmployeeRepository
+    {
+        Task<bool> AddEmployeeAsync(Employee employee);
+        Task<bool> EditEmployee(Employee employee);
+        Task<Employee> GetEmployeeById(Guid employeeId);
+        Task<List<Employee>> GetAllEmployees();
+        Task<bool> DeleteEmployee(Guid employeeId);
+    }
+}
