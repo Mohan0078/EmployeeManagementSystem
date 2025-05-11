4034ab4f46476d980676455d346f2440ae788ce2
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-05-11 13:15:42 +0530

    feat: employee db context

---
+++ EmployeeManagement.API/EmployeeManagement.DatabaseEntities/Models/employeemanagementContext.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EmployeeManagement.DatabaseEntities.Models
+{
+    public partial class employeemanagementContext : DbContext
+    {
+        public employeemanagementContext()
+        {
+        }
+
+        public employeemanagementContext(DbContextOptions<employeemanagementContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; } = null!;
+        public virtual DbSet<Member> Members { get; set; } = null!;
+        public virtual DbSet<State> States { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseMySql("server=localhost;database=employeemanagement;user=test;password=test", Microsoft.EntityFrameworkCore.ServerVersion.Parse("5.7.39-mysql"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseCollation("latin1_swedish_ci")
+                .HasCharSet("latin1");
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.ToTable("employee");
+
+                entity.Property(e => e.DateOfJoin).HasColumnType("datetime");
+
+                entity.Property(e => e.Designation).HasMaxLength(250);
+
+                entity.Property(e => e.IsDeleted).HasMaxLength(45);
+
+                entity.Property(e => e.Salary).HasPrecision(10);
+            });
+
+            modelBuilder.Entity<Member>(entity =>
+            {
+                entity.ToTable("member");
+
+                entity.Property(e => e.DateOfBirth).HasColumnType("datetime");
+
+                entity.Property(e => e.Name).HasMaxLength(250);
+            });
+
+            modelBuilder.Entity<State>(entity =>
+            {
+                entity.ToTable("state");
+
+                entity.Property(e => e.StateCode).HasMaxLength(10);
+
+                entity.Property(e => e.StateName).HasMaxLength(250);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
