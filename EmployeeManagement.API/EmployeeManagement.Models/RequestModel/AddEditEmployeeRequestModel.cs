8071e6cb553c28138839d1e42232ee9849be50db
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-05-13 00:57:13 +0530

    feat: add edit employee request model

---
+++ EmployeeManagement.API/EmployeeManagement.Models/RequestModel/AddEditEmployeeRequestModel.cs
@@ -0,0 +1,16 @@
+﻿namespace EmployeeManagement.Models.RequestModel
+{
+    public class AddEditEmployeeRequestModel
+    {
+        public Guid? EmployeeId { get; set; }
+        public Guid? MemberId { get; set; }
+        public string Name { get; set; }
+        public string? Designation { get; set; }
+        public string? Gender { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public DateTime? DateOfJoin { get; set; }
+        public decimal? Salary { get; set; }
+        public Guid? StateId { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
