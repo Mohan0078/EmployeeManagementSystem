f2899a04560709f354cb4a6f6d924be87d35f4f0
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-05-13 00:41:48 +0530

    feat: cors and service registration

--- EmployeeManagement.API/EmployeeManagement.API/Program.cs
+++ EmployeeManagement.API/EmployeeManagement.API/Program.cs
@@ -1,25 +1,50 @@
+using EmployeeManagement.BLL.Interfaces;
+using EmployeeManagement.BLL.Services;
+using EmployeeManagement.DAL.RepositoryImplementation;
+using EmployeeManagement.DAL.RepositoryInterfaces;
+using EmployeeManagement.DatabaseEntities.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Register MySQL DbContext
+builder.Services.AddDbContext<employeemanagementContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
+    ));
+
 // Add services to the container.
+builder.Services.AddTransient<IStateRepository, StateRepository>();
+builder.Services.AddTransient<IStateService, StateService>();
+
+// CORS: register with explicit origin (127.0.0.1:5500 is from Live Server)
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://127.0.0.1:5500")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseCors("AllowFrontend");
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
