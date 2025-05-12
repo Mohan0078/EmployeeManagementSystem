09a1ffef6aabac8184f0334b61135d90faa5b3a3
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-05-13 00:39:35 +0530

    feat: employee contreoller

---
+++ EmployeeManagement.API/EmployeeManagement.API/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using EmployeeManagement.BLL.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetEmployeeList()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
